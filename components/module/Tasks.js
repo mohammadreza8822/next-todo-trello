806e0fa42ebb00bb38d2262cca8c74833f583b15
Mohammadreza <ma3968312@gmail.com>
2025-04-23 21:53:50 +0330

    update todos status

--- components/module/Tasks.js
+++ components/module/Tasks.js
@@ -2,6 +2,15 @@ import { RiMastodonLine } from "react-icons/ri";
 import { BiRightArrow, BiLeftArrow } from "react-icons/bi";
 
 function Tasks({ data, next, back, fetchTodos }) {
+  const changeStatus = async (id, status) => {
+    const res = await fetch("/api/todos", {
+      method: "PATCH",
+      body: JSON.stringify({ id, status }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    if (data.status === "success") fetchTodos();
+  };
   return (
     <div className="tasks">
       {data?.map((item) => (
@@ -11,13 +20,19 @@ function Tasks({ data, next, back, fetchTodos }) {
           <h4>{item.title}</h4>
           <div>
             {back ? (
-              <button className="button-back">
+              <button
+                className="button-back"
+                onClick={() => changeStatus(item._id, back)}
+              >
                 <BiLeftArrow />
                 Back
               </button>
             ) : null}
             {next ? (
-              <button className="button-next">
+              <button
+                className="button-next"
+                onClick={() => changeStatus(item._id, next)}
+              >
                 Next
                 <BiRightArrow />
               </button>
