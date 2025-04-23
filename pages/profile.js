5c644678185ba128e073f3849fcb2f992b502d7c
Mohammadreza <ma3968312@gmail.com>
2025-04-23 22:45:37 +0330

    show data in profile

---
+++ pages/profile.js
@@ -0,0 +1,7 @@
+import ProfilePage from "@/components/templates/ProfilePage";
+
+function Profile() {
+  return <ProfilePage />;
+}
+
+export default Profile;
--- pages/signout.js
+++
@@ -1,8 +0,0 @@
-import SignOutPage from "@/components/templates/SignOutPage";
-import React from "react";
-
-function SignOut() {
-  return <SignOutPage />;
-}
-
-export default SignOut;
