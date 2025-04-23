ef90d1e8c10ce1e1c25c46ad19fc2047d041c101
Mohammadreza <ma3968312@gmail.com>
2025-04-23 22:53:25 +0330

    access to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import HomePage from "@/components/templates/HomePage";
+import { getSession } from "next-auth/react";
 
 export default function Home() {
   return (
@@ -6,4 +7,21 @@ export default function Home() {
       <HomePage />
     </>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
